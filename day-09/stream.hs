df5b6617761a03be94fe2758868fc197b7b23843
Joseph Shaw <j.g.shaw95@gmail.com>
2017-12-09 22:13:25 +0000

    Complete part 2 for counting the removed garbage from the stream

--- day-09/stream.hs
+++ day-09/stream.hs
@@ -7,12 +7,12 @@ scoreGroup :: Int -> Int -> Int -> String -> (Int, Int)
 scoreGroup tScore tGarbage  _ [] = (tScore, tGarbage)
 scoreGroup tScore tGarbage score ('{' : tail) = scoreGroup (tScore + score) tGarbage  (score + 1) tail
 scoreGroup tScore tGarbage score ('}' : tail) = scoreGroup tScore tGarbage (score - 1) tail
-scoreGroup tScore tGarbage score ('<' : tail) = scoreGroup tScore tGarbage score (removeGarbage tail)
+scoreGroup tScore tGarbage score ('<' : tail) = scoreGroup tScore (tGarbage + garbageCount) score clean
+  where
+    (garbageCount, clean) = removeGarbage 0 tail
 scoreGroup tScore tGarbage score (_ : tail) = scoreGroup tScore tGarbage score tail
 
-removeGarbage :: String -> String
-removeGarbage ('>' : tail) = tail
-removeGarbage ('!' : _ : tail) = removeGarbage tail
-removeGarbage (_ : tail) = removeGarbage tail
-
-
+removeGarbage :: Int -> String -> (Int, String)
+removeGarbage count ('>' : tail) = (count, tail)
+removeGarbage count ('!' : _ : tail) = removeGarbage count tail
+removeGarbage count (_ : tail) = removeGarbage (count + 1) tail
--- day-09/test.hs
+++ day-09/test.hs
@@ -2,7 +2,7 @@ import Test.HUnit
 import Stream
 
 testScore expected input = TestCase $ assertEqual "" expected (fst $ Stream.score input)
-
+testGarbageCount expected input = TestCase $ assertEqual "" expected (snd $ Stream.score input)
 
 tests = TestList [TestLabel "A single group has a score of 1" (testScore 1 "{}"),
                   TestLabel "Nested groups are scored with increasing counts" (testScore 6 "{{{}}}"),
@@ -11,5 +11,12 @@ tests = TestList [TestLabel "A single group has a score of 1" (testScore 1 "{}")
                   TestLabel "Garbage is ignored" (testScore 1 "{<a>,<a>,<a>,<a>}"), 
                   TestLabel "Garbage in a nested group is ignored" (testScore 9 "{{<ab>},{<ab>},{<ab>},{<ab>}}"),
                   TestLabel "A group inside garbage is ignored" (testScore 1 "{<{}>}"),
-                  TestLabel "Escape characters ignore closing garbage tags" (testScore 3 "{{<a!>},{<a!>},{<a!>},{<ab>}}")
+                  TestLabel "Escape characters ignore closing garbage tags" (testScore 3 "{{<a!>},{<a!>},{<a!>},{<ab>}}"),
+                  
+                  TestLabel "Empty garbage has a count of zero" (testGarbageCount 0 "{<>}"),
+                  TestLabel "Non-special garbage characters are counted" (testGarbageCount 17 "{<random charactors>}"),
+                  TestLabel "Opening garbage tags are counted" (testGarbageCount 3 "{<<<<>}"),
+                  TestLabel "Group tags inside garbage are counted" (testGarbageCount 2 "{<{}>}"),
+                  TestLabel "Escaped characters are not counted" (testGarbageCount 0 "{<!!>}"),
+                  TestLabel "Multiple escaped characters are not counted" (testGarbageCount 0 "{<!!!>>}")
                  ]
