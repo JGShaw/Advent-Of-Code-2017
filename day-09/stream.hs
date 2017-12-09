a5195cbb43e98ade8b9d02e368bd22c30aeccfef
Joseph Shaw <j.g.shaw95@gmail.com>
2017-12-09 21:40:07 +0000

    working solution

--- day-09/stream.hs
+++ day-09/stream.hs
@@ -1,4 +1,16 @@
 module Stream where
 
 score :: String -> Int
-score s = 0
+score input = scoreGroup 1 input
+
+scoreGroup :: Int -> String -> Int
+scoreGroup _ [] = 0
+scoreGroup score ('{' : tail) = score + (scoreGroup (score + 1) tail)
+scoreGroup score ('}' : tail) = scoreGroup (score - 1) tail
+scoreGroup score ('<' : tail) = scoreGroup score (removeGarbage tail)
+scoreGroup score (_ : tail) = scoreGroup score tail
+
+removeGarbage :: String -> String
+removeGarbage ('>' : tail) = tail
+removeGarbage ('!' : _ : tail) = removeGarbage tail
+removeGarbage (_ : tail) = removeGarbage tail
--- day-09/test.hs
+++ day-09/test.hs
@@ -1,9 +1,15 @@
 import Test.HUnit
 import Stream
 
-testScore expected input = TestCase $ assertEqual "" expected $ Stream.score input
+testScore expected input = TestCase $ assertEqual "" expected (Stream.score input)
 
 
 tests = TestList [TestLabel "A single group has a score of 1" (testScore 1 "{}"),
-                  TestLabel "Nested groups are scored with increasing counts" (testScore 6 "{{{}}}")
+                  TestLabel "Nested groups are scored with increasing counts" (testScore 6 "{{{}}}"),
+                  TestLabel "Comma seperated groups are scored with the same count" (testScore 5 "{{},{}}"),
+                  TestLabel "Nested and comma seperated groups" (testScore 16 "{{{},{},{{}}}}"),
+                  TestLabel "Garbage is ignored" (testScore 1 "{<a>,<a>,<a>,<a>}"), 
+                  TestLabel "Garbage in a nested group is ignored" (testScore 9 "{{<ab>},{<ab>},{<ab>},{<ab>}}"),
+                  TestLabel "A group inside garbage is ignored" (testScore 1 "{<{}>}"),
+                  TestLabel "Escape characters ignore closing garbage tags" (testScore 3 "{{<a!>},{<a!>},{<a!>},{<ab>}}")
                  ]
