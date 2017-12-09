6f66706373911849f2dab4df9f08822ad6362d7c
Joseph Shaw <j.g.shaw95@gmail.com>
2017-12-09 23:56:22 +0000

    Remove un-needed brackets

--- day-01/captcha.hs
+++ day-01/captcha.hs
@@ -6,10 +6,10 @@ solveP1 :: String -> Int
 solveP1 input = solve' input (rotate 1 input)
 
 solveP2 :: String -> Int
-solveP2 input = solve' input (rotate ((length input) `div` 2) input)
+solveP2 input = solve' input $ rotate (length input `div` 2) input
 
 solve' :: String -> String -> Int
 solve' in1 in2 = sum $ map (\(x1, x2) -> if x1 == x2 then (Char.digitToInt x1) else 0) $ zip in1 in2
 
 rotate :: Int -> String -> String
-rotate n s = take (length s)  (drop n (cycle s))
+rotate n s = take (length s) $ drop n $ cycle s
