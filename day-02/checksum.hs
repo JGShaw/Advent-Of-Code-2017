cdd1402744bd35d2ab17d05853749894476d03c2
Joseph Shaw <j.g.shaw95@gmail.com>
2017-12-10 13:17:30 +0000

    Solution to part 2

--- day-02/checksum.hs
+++ day-02/checksum.hs
@@ -1,11 +1,32 @@
 module Checksum where
 
-checksum :: String -> Int
-checksum input = sum $ map (\line -> (maximum $ map readInt line) - (minimum $ map readInt line)) (map words (lines input))
+import Data.List
+
+checksum1 :: String -> Int
+checksum1 input = sum $ map (\line -> maximum line - minimum line) $ linesToInts input
+
+checksum2 :: String -> Int
+checksum2 input = sum $ map checksum2' $ linesToInts input
+
+checksum2' :: [Int] -> Int
+checksum2' ints = val1 `div` val2
+  where
+    (val1 : val2 : _) = head $ filter (\(x1 : x2 : _) -> x1 `mod` x2 == 0) $ allPairs ints
+
+allPairs :: [Int] -> [[Int]]
+allPairs ints = concatMap permutations $ filter (\subseq -> length subseq == 2) $ subsequences ints
+
+
+linesToInts :: String -> [[Int]]
+linesToInts input = map (\line -> map readInt line) $ map words $ lines input
 
 readInt :: String -> Int
 readInt s = read s :: Int
 
-readInput input = do
+readInput1 input = do
   file <- readFile input
-  print $ checksum file 
+  print $ checksum1 file 
+
+readInput2 input = do
+  file <- readFile input
+  print $ checksum2 file 
--- day-02/test.hs
+++ day-02/test.hs
@@ -1,9 +1,12 @@
 import Test.HUnit
 import Checksum
 
-testChecksum expected input = TestCase $ assertEqual "" expected (Checksum.checksum input)
+testChecksum1 expected input = TestCase $ assertEqual "" expected (Checksum.checksum1 input)
+testChecksum2 expected input = TestCase $ assertEqual "" expected (Checksum.checksum2 input)
 
-tests = TestList [TestLabel "Case 1" (testChecksum 18 "5\t1\t9\t5\n7\t5\t3\n2\t4\t6\t8"),
-                 TestLabel "Case 2" (testChecksum 15 "5\t5\t5\n1\t2\t3\t4\t5\t6\n10\t0\t5\t7\t4\t2\t6\t8\t9\t5\t3\t2\t5\t4\t5\t6\t7\t8")
+tests = TestList [TestLabel "Case 1 P1" (testChecksum1 18 "5\t1\t9\t5\n7\t5\t3\n2\t4\t6\t8"),
+                  TestLabel "Case 2 P1" (testChecksum1 15 "5\t5\t5\n1\t2\t3\t4\t5\t6\n10\t0\t5\t7\t4\t2\t6\t8\t9\t5\t3\t2\t5\t4\t5\t6\t7\t8"),
+
+                  TestLabel "Case 1 P2" (testChecksum2 9 "5\t9\t2\t8\n9\t4\t7\t3\n3\t8\t6\t5")
                  ]
                  
