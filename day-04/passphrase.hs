375ba11647f73368db2cca88df0d50e3c7a7af69
Joseph Shaw <j.g.shaw95@gmail.com>
2017-12-11 21:26:09 +0000

    Working solution to part 2

--- day-04/passphrase.hs
+++ day-04/passphrase.hs
@@ -1,11 +1,19 @@
 module Passphrase where
 
 import qualified Data.Set as Set
+import qualified Data.List as List
 
 checkDuplicates :: String -> Bool
 checkDuplicates input = (length splitWords) == (length $ Set.fromList splitWords)
   where
     splitWords = words input
+
+checkAnagrams :: String -> Bool
+checkAnagrams input = (length $ uniquePerms) == (length $ Set.fromList uniquePerms)
+  where
+    splitWords = words input
+    permutations = map List.permutations splitWords
+    uniquePerms = concatMap (\x -> Set.toList $ Set.fromList x) permutations
 
 checkLines :: (String -> Bool) -> String -> Int
 checkLines checker input = length $ filter checker $ lines input 
--- day-04/test.hs
+++ day-04/test.hs
@@ -3,11 +3,18 @@ import Passphrase
 
 testCheckDuplicates expected input = TestCase $ assertEqual "" expected (Passphrase.checkDuplicates input)
 testCheckLines expected input = TestCase $ assertEqual "" expected (Passphrase.checkLines checkDuplicates input)
+testCheckAnagrams expected input = TestCase $ assertEqual "" expected (Passphrase.checkAnagrams input)
 
-tests = TestList [TestLabel "Case 1" (testCheckDuplicates True  "aa bb cc dd ee"),
-                  TestLabel "Case 2" (testCheckDuplicates False  "aa bb cc dd aa"),
-                  TestLabel "Case 3" (testCheckDuplicates True  "aa bb cc dd aaa"),
+tests = TestList [TestLabel "Duplicates Case 1" (testCheckDuplicates True  "aa bb cc dd ee"),
+                  TestLabel "Duplicates Case 2" (testCheckDuplicates False  "aa bb cc dd aa"),
+                  TestLabel "Duplicates Case 3" (testCheckDuplicates True  "aa bb cc dd aaa"),
 
-                  TestLabel "Check lines" (testCheckLines 2 "aa bb cc\naa bb aa\naa bb aaa")
+                  TestLabel "Check lines" (testCheckLines 2 "aa bb cc\naa bb aa\naa bb aaa"),
+
+                  TestLabel "Anagrams Case 1" (testCheckAnagrams True "abcde fghij"),
+                  TestLabel "Anagrams Case 2" (testCheckAnagrams False "abcde xyz ecdab"),
+                  TestLabel "Anagrams Case 3" (testCheckAnagrams True "a ab abc abd abf abj"),
+                  TestLabel "Anagrams Case 4" (testCheckAnagrams True "iiii oiii ooii oooi oooo"),
+                  TestLabel "Anagrams Case 5" (testCheckAnagrams False "oiii ioii iioi iiio")
                  ]
                  
