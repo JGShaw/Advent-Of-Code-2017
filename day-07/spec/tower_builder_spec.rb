b4fd04191939c3e8f2527c5adaaa29536b780702
Joseph Shaw <j.g.shaw95@gmail.com>
2017-12-18 19:34:32 +0000

    Add tests for part 2

--- day-07/spec/tower_builder_spec.rb
+++ day-07/spec/tower_builder_spec.rb
@@ -4,28 +4,31 @@ require "program"
 describe TowerBuilder do
   subject { TowerBuilder.new }
 
-  it "Can take a series of lines and find the correct root" do
-    lines = []
-    lines.push("pbga (66)")
-    lines.push("xhth (57)")
-    lines.push("ebii (61)")
-    lines.push("havc (66)")
-    lines.push("ktlj (57)")
-    lines.push("fwft (72) -> ktlj, cntj, xhth")
-    lines.push("qoyq (66)")
-    lines.push("padx (45) -> pbga, havc, qoyq")
-    lines.push("tknk (41) -> ugml, padx, fwft")
-    lines.push("jptl (61)")
-    lines.push("ugml (68) -> gyxo, ebii, jptl")
-    lines.push("gyxo (61)")
-    lines.push("cntj (57)")
-    expect(subject.find_root lines).to eq "tknk"
+  before(:each) do
+    @lines = []
+    @lines.push("pbga (66)")
+    @lines.push("xhth (57)")
+    @lines.push("ebii (61)")
+    @lines.push("havc (66)")
+    @lines.push("ktlj (57)")
+    @lines.push("fwft (72) -> ktlj, cntj, xhth")
+    @lines.push("qoyq (66)")
+    @lines.push("padx (45) -> pbga, havc, qoyq")
+    @lines.push("tknk (41) -> ugml, padx, fwft")
+    @lines.push("jptl (61)")
+    @lines.push("ugml (68) -> gyxo, ebii, jptl")
+    @lines.push("gyxo (61)")
+    @lines.push("cntj (57)")
   end
-  
+
+  it "Can take a series of lines and find the correct root" do
+    expect(subject.find_root @lines).to eq "tknk"
+  end
+
   it "Can extract the program name" do
     expect(subject.extract_name "fwft (72) -> ktlj, cntj, xhth").to eq "fwft"
   end
- 
+
 
   it "Can extract the program weight" do
     expect(subject.extract_weight "fwft (72) -> ktlj, cntj, xhth").to eq 72
@@ -33,6 +36,11 @@ describe TowerBuilder do
 
   it "Can extract the program name" do
     expect(subject.extract_subprograms "fwft (72) -> ktlj, cntj, xhth").to contain_exactly("ktlj", "cntj", "xhth")
+  end
+
+  it "Can work out the valued that needs to be added to balance the tower" do
+    root = subject.find_root(@lines)
+    expect(subject.weight_needed_to_balance root).to eq 8
   end
 
   it "adding subprograms sets the subprogram list correctly" do
