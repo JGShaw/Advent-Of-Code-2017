881b279d2ab070d6b7c2a7df4e6c29086e1b0826
Joseph Shaw <j.g.shaw95@gmail.com>
2017-12-20 19:23:34 +0000

    Finished part two ruby version

--- day-03/part-two-ruby/lib/spiral.rb
+++ day-03/part-two-ruby/lib/spiral.rb
@@ -1,3 +1,67 @@
 class Spiral
 
+  def first_value_larger_than input
+    x = 0
+    y = 0
+    @memory = Hash.new { 0 }
+    @memory[[x, y]] = 1
+    directions = [:right, :up, :left, :down]
+
+    x += 1
+
+    largest = 1
+    while largest <= input do
+  
+      memoryValue = calculate_memory_value(x, y)
+      @memory[[x, y]] = memoryValue    
+      largest = memoryValue if memoryValue > largest
+     
+      case directions[0]
+      when :right
+        if @memory.has_key? [x, y + 1] then
+          x += 1
+        else
+          y += 1
+          directions.rotate!
+        end
+      when :up
+        if @memory.has_key? [x - 1, y] then
+          y += 1
+        else
+          x -= 1
+          directions.rotate!
+        end
+      when :left
+        if @memory.has_key? [x, y - 1] then
+          x -= 1
+        else
+          y -= 1
+          directions.rotate!
+        end
+      when :down
+        if @memory.has_key? [x + 1, y] then
+          y -= 1
+        else
+          x += 1
+          directions.rotate!
+        end
+      end
+
+    end
+    return largest
+  end
+
+  private
+  
+  def calculate_memory_value(x, y)
+    @memory[[x + 1, y]] + @memory[[x - 1, y]] + @memory[[x, y + 1]] + @memory[[x, y - 1]] +
+    @memory[[x + 1, y + 1]] + @memory[[x + 1, y - 1]] + @memory[[x - 1, y + 1]] + @memory[[x - 1, y - 1]]
+  end
+
+end
+
+
+if __FILE__ == $0 then
+  spiral = Spiral.new
+  puts spiral.first_value_larger_than(File.open(ARGV[0]).first.to_i)
 end
--- day-03/part-two-ruby/spec/spiral_spec.rb
+++ day-03/part-two-ruby/spec/spiral_spec.rb
@@ -2,5 +2,24 @@ require "spiral"
 
 describe Spiral do
   subject { Spiral.new }
+ 
+  it "The first value larger than 12 is 23" do
+    expect(subject.first_value_larger_than 12).to eq 23
+  end
+
+  it "The first value larger than 25 is 26" do
+    expect(subject.first_value_larger_than 25).to eq 26
+  end
   
+  it "The first value larger than 70 is 122" do
+    expect(subject.first_value_larger_than 70).to eq 122
+  end
+
+  it "The first value larger than 200 is 304" do
+    expect(subject.first_value_larger_than 200).to eq 304
+  end
+  
+  it "The first value larger than 800 is 806" do
+    expect(subject.first_value_larger_than 800).to eq 806
+  end
 end
