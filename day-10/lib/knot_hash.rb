13a5daffc5a27095ee62a9e35ebab77c3b84daa1
Joseph Shaw <j.g.shaw95@gmail.com>
2017-12-21 12:25:47 +0000

    Add part two

--- day-10/lib/knot_hash.rb
+++ day-10/lib/knot_hash.rb
@@ -2,27 +2,73 @@ class KnotHash
 
   def initialize(size)
     @loop = (0..size - 1).to_a
+    @skipSize = 0
+    @rotated = 0
   end
 
   def simple_hash(lengths)
-    skipSize = 0
-    rotated = 0
+    run_hash_rounds(1, lengths)
+  end
 
+  def complex_hash(input)
+    lengths = ascii_to_ints(input)
+    lengths = append_suffix(lengths)
+    run_hash_rounds(64, lengths)
+    denseHash = create_dense_hash(16, @loop)
+    return ints_to_hex(denseHash)
+  end
+
+  def run_hash_rounds(count, lengths)
+    count.times do
+      hash(lengths)
+    end
+    return @loop.rotate!(@loop.length - (@rotated % @loop.length))
+  end
+
+  def ascii_to_ints(input)
+    input.bytes
+  end
+
+  def append_suffix(input)
+    input + [17, 31, 73, 47, 23]
+  end
+
+  def create_dense_hash(groupSize, input)
+    output = []
+    input.each_slice(groupSize) do |slice|
+      output << slice.reduce(:^)
+    end
+    return output
+  end
+
+  def ints_to_hex(input)
+    output = ""
+    input.collect do |num|
+      output += num.to_s(16).rjust(2, '0')
+    end
+    return output
+  end
+
+  private
+  def hash(lengths) 
     lengths.each do |length| 
       reversedElements = @loop.take(length).reverse
       remainingElements = @loop.drop(length)
       @loop = reversedElements + remainingElements
-      @loop.rotate!(length + skipSize)
-      rotated += length + skipSize
-      skipSize+=1
+      @loop.rotate!(length + @skipSize)
+      @rotated += length + @skipSize
+      @skipSize+=1
     end
-    return @loop.rotate!(@loop.length - (rotated % @loop.length))
   end
+
 end
 
 if __FILE__ == $0 then
   knotHash = KnotHash.new 256
-  lengths = File.open(ARGV[0]).first.split(",").map(&:to_i)
-  simple_hashed = knotHash.simple_hash(lengths)
-  puts "Product of first two: #{simple_hashed[0] *  simple_hashed[1]}"
+  lengths = File.open(ARGV[0]).first
+  simple_hashed = knotHash.simple_hash(lengths.split(",").map(&:to_i))
+  puts "Product of first two for the simple hash: #{simple_hashed[0] *  simple_hashed[1]}"
+  knotHash = KnotHash.new 256
+  puts "The complex hash is: #{knotHash.complex_hash(lengths.strip)}"
+
 end
--- day-10/spec/knot_hash_spec.rb
+++ day-10/spec/knot_hash_spec.rb
@@ -1,22 +1,60 @@
 require "knot_hash"
 
 describe KnotHash do
-  subject { KnotHash.new 5 }
 
-  it "The provided example gives the correct output for one length" do
-    expect(subject.simple_hash [3]).to eq [2, 1, 0, 3, 4]
+  context "Simple hash" do
+    subject { KnotHash.new 5 }
+    it "The provided example gives the correct output for one length" do
+      expect(subject.simple_hash [3]).to eq [2, 1, 0, 3, 4]
+    end
+
+    it "The provided example gives the correct output for two lengths" do
+      expect(subject.simple_hash [3, 4]).to eq [4, 3, 0, 1, 2]
+    end
+
+    it "The provided example gives the correct output for three lengths" do
+      expect(subject.simple_hash [3, 4, 1]).to eq [4, 3, 0, 1, 2]
+    end
+
+    it "The provided example gives the correct output for all lengths" do
+      expect(subject.simple_hash [3, 4, 1, 5]).to eq [3, 4, 2, 1, 0]
+    end
   end
 
-  it "The provided example gives the correct output for two lengths" do
-    expect(subject.simple_hash [3, 4]).to eq [4, 3, 0, 1, 2]
-  end
+  context "Complex hash" do
+    subject { KnotHash.new 256 }
+    it "Provided example: empty string" do
+      expect(subject.complex_hash "").to eq "a2582a3a0e66e6e86e3812dcb672a272"
+    end
 
-  it "The provided example gives the correct output for three lengths" do
-    expect(subject.simple_hash [3, 4, 1]).to eq [4, 3, 0, 1, 2]
-  end
+    it "Provided example: AoC 2017" do
+      expect(subject.complex_hash "AoC 2017").to eq "33efeb34ea91902bb2f59c9920caa6cd"
+    end
 
-  it "The provided example gives the correct output for all lengths" do
-    expect(subject.simple_hash [3, 4, 1, 5]).to eq [3, 4, 2, 1, 0]
+    it "Provided example: 1,2,3" do
+      expect(subject.complex_hash "1,2,3").to eq "3efbe78a8d82f29979031a4aa0b16a9d"
+    end
+
+    it "Provided example: 1,2,4" do
+      expect(subject.complex_hash "1,2,4").to eq "63960835bcdc130f0b66d7ff4f6a5a8e"
+    end
+
+    it "Can convert from a string to an array of ascii values" do
+      expect(subject.ascii_to_ints "1,2,3").to eq [49, 44, 50, 44, 51]
+    end
+
+    it "Can append a constant suffix to an input" do
+      expect(subject.append_suffix [49, 44, 50, 44, 51]).to eq [49, 44, 50, 44, 51, 17, 31, 73, 47, 23]
+    end
+
+    it "Can create a dense hash" do
+      expect(subject.create_dense_hash(4, (1..16).to_a)).to eq [4, 12, 4, 28]
+    end
+
+    it "Can convert a list of integers to hex output" do
+      expect(subject.ints_to_hex [64, 7, 255]).to eq "4007ff"
+    end
+
   end
 
 end
