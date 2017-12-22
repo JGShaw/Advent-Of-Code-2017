cddbeb88e54edceb872766bdd724153cdd420717
Joseph Shaw <j.g.shaw95@gmail.com>
2017-12-22 14:27:12 +0000

    Add part two code

--- day-12/lib/search.rb
+++ day-12/lib/search.rb
@@ -1,7 +1,30 @@
 require 'set'
 
 class Search
+  
+  def count_groups(connections)
+    remainingPrograms = Set.new
+    connections.each do |connection|
+      remainingPrograms << connection.match(/\d+/)[0]
+    end
+ 
+    groups = 0
+    until remainingPrograms.empty? do
+      groups += 1
+      arr = remainingPrograms.to_a
+      nextProgram = arr.pop
+      remainingPrograms = Set.new arr
+      connected = get_connected_group(nextProgram, connections)
+      remainingPrograms = remainingPrograms - connected
+    end
+    return groups
+  end
+  
   def count_connected(source, connections)
+    return get_connected_group(source, connections).size
+  end
+
+  def get_connected_group(source, connections)
     explored = Set.new []
     frontier = [source]
 
@@ -13,7 +36,7 @@ class Search
       end
       explored << current
     end
-    return explored.size
+    return explored
   end
 
   def extract_connections(input) 
@@ -26,4 +49,5 @@ if __FILE__ == $0 then
   search = Search.new
   connections = File.open(ARGV[0]).to_a
   puts "Connected to zero: #{search.count_connected("0", connections)}"
+  puts "Number of groups: #{search.count_groups(connections)}"
 end
--- day-12/spec/search_spec.rb
+++ day-12/spec/search_spec.rb
@@ -3,7 +3,6 @@ require "search"
 describe Search do
   subject { Search.new }
   
-  it "count_connected works correctly for the part one example" do
     input = ["0 <-> 2",
             "1 <-> 1",
             "2 <-> 0, 3, 4",
@@ -11,7 +10,13 @@ describe Search do
             "4 <-> 2, 3, 6",
             "5 <-> 6",
             "6 <-> 4, 5"]
+  
+    it "count_connected works correctly for the part one example" do
     expect(subject.count_connected("0", input)).to eq 6
+  end
+
+  it "can count the number of connected groups" do
+    expect(subject.count_groups(input)).to eq 2
   end
 
 end
