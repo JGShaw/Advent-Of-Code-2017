04221250f22df26c309edc7cfeb40d4a212455bc
Joseph Shaw <j.g.shaw95@gmail.com>
2017-12-23 17:01:07 +0000

    Add solution to part two

--- day-14/lib/defrag.rb
+++ day-14/lib/defrag.rb
@@ -11,10 +11,29 @@ class Defrag
   end
 
   def count_groups(input)
-    0
+    hashes = generate_binary_hashes(128, input)
+    count = 0
+
+    hashes.length.times do |hashNum|
+      hashes[hashNum].length.times do |charNum|
+        if hashes[hashNum][charNum] == '1'
+          count += 1
+          zero_group(hashes, hashNum, charNum)
+        end
+      end
+    end
+
+    #hashes.each_with_index do |hash,hashNum|
+    #  hash.each_char.with_index do |char,charNum|
+    #    if char == '1' then
+    #      zero_group(hashes, hashNum, charNum)
+    #      count += 1
+    #    end
+    #  end
+    #end
+    return count
   end
 
-  private
 
   def generate_binary_hashes(count, input)
     hashes = []
@@ -25,10 +44,23 @@ class Defrag
     end
     return hashes
   end
+
+  def zero_group(hashes, hashNum, charNum)
+    if hashes[hashNum][charNum] == '1' then
+      hashes[hashNum][charNum] = '0'
+
+      zero_group(hashes, hashNum + 1, charNum) if (hashNum + 1) < hashes.length 
+      zero_group(hashes, hashNum - 1, charNum) if hashNum > 0
+      zero_group(hashes, hashNum, charNum + 1) if (charNum + 1) < hashes[hashNum].length
+      zero_group(hashes, hashNum, charNum - 1) if charNum > 0
+    end
+  end
+
 end
 
 if __FILE__ == $0 then
   defrag = Defrag.new
   input = File.open(ARGV[0]).first.strip!
   puts "Used squares: #{defrag.count_used(input)}"
+  puts "Groups: #{defrag.count_groups(input)}"
 end
